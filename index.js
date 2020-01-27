90bfc72b36df381608ac9fa5354f3a9bf19b301b
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-27 13:42:56 -0300

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs =  require('fs');
-const http = require('http')
+const http = require('http');
+const url = require('url');
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -36,7 +37,19 @@ console.log('Will read file');
 ///////////////////////////////////////////////////////////
 ///////SERVER
  const server = http.createServer((req, res)=>{
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName ==='/overview'){
+        res.end('This is the OVERVIEW');
+    }else if (pathName === '/product'){
+        res.end('This is the PRODUCT');
+    }else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html',
+            'my-own-header': 'hello-word'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () =>{
