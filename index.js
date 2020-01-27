027d55b515d6ed05fd38efd4ce6f29071a0a769b
IgorFerro <igor.ferro.parreira@gmail.com>
2020-01-27 20:24:35 -0300

    Applying slugify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs =  require('fs');
 const http = require('http');
 const url = require('url');
-
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replacleTemplate');
 
 
@@ -44,6 +44,9 @@ console.log('Will read file');
  
  const data= fs.readFileSync(`${__dirname}//1-node-farm//starter//dev-data//data.json`, 'utf-8');
  const dataObj = JSON.parse(data);
+
+ const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+ console.log(slugs);
 
  const server = http.createServer((req, res)=>{
     const {query, pathname} = url.parse(req.url,true);
