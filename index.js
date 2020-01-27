ba957e54d9b783b4a39e0e6d4a5d7ce3f357ea2d
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-27 14:31:50 -0300

    Building Simple API

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ console.log('Will read file');
 
 ///////////////////////////////////////////////////////////
 ///////SERVER
+
+ const data= fs.readFileSync(`${__dirname}//1-node-farm//starter//dev-data//data.json`, 'utf-8')
+ const dataObj = JSON.parse(data);
+
  const server = http.createServer((req, res)=>{
     const pathName = req.url;
 
@@ -43,6 +47,9 @@ console.log('Will read file');
         res.end('This is the OVERVIEW');
     }else if (pathName === '/product'){
         res.end('This is the PRODUCT');
+    }else if (pathName === '/api'){
+     res.writeHead(200, {'Content-type': 'application/json'});
+     res.end(data);   
     }else {
         res.writeHead(404, {
             'Content-type' : 'text/html',
