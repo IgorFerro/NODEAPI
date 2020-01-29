a7e99fbf7a8cdc5cf26ff7ae566308e8d25b664e
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-29 07:17:41 -0300

    Events in Practice

---
+++ 2-how-node-works/starter/events.js
@@ -0,0 +1,47 @@
+const EventEmitter = require("events");
+const http= require("http");
+
+class Sales extends EventEmitter{
+ constructor(){
+     super();
+ }
+}
+
+const myEmitter = new  Sales();
+
+myEmitter.on("newSale", () => {
+    console.log("There was a new sale!")
+});
+
+myEmitter.on("newSale", ()=>{
+    console.log("Costumer name: Jonas");
+});
+myEmitter.on("newSale", stock => {
+    console.log(`There are now ${stock} items left in stock`);
+})
+
+myEmitter.emit("newSale", 9);
+
+////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) =>{
+    console.log("Request received!");
+    console.log(req.url)
+    res.end("Request Received");
+
+});
+
+server.on("request", (req, res) =>{
+   console.log("Another Request");
+});
+
+server.on("close", (req, res) =>{
+    res.end("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () =>{
+    console.log("Waiting for request...")
+});
+
