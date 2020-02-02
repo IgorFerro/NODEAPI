9eff21be7464937b30d5ce6c5144f75f226eccbe
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-02-02 06:50:45 -0300

    Handling POST Requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 //Routing
 /*app.get('/',(req,res)=>{
     res.status(200)
@@ -24,6 +26,23 @@ app.get('/api/v1/tours', (req,res) =>{
       }
   })
 });
+
+app.post('/api/v1/tours', (req,res)=>{
+    //console.log(req.body);
+    const newId = tours[tours.length -1].id +1;
+    const newTour = Object.assign({id:newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours.json`,JSON.stringify(tours), err =>{
+    res.status(201),json({
+        status: 'sucess',
+        data:{
+            tour: newTour
+        }
+    })
+  })
+})
+
 
 
 const port = 3001;
