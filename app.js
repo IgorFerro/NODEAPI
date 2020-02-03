2054821bdda9efd8ab300f82abf2bdd0e85316f7
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-02-03 07:59:32 -0300

    Handling Patch Requests

--- app.js
+++ app.js
@@ -67,7 +67,24 @@ app.post('/api/v1/tours', (req,res)=>{
         }
     })
   });
+});
+
+//Make Patch
+app.patch('/api/v1/tours/:id', (req , res) => {
+     if(req.params.id * 2 > tours.length){
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Invalid ID'
+            });
+        }
+    res.status(200).json({
+        status: 'success',
+        data:{
+            tour: '<Updated tour here...>'
+        }
+    })
 })
+
 
 const port = 3001;
 app.listen(port, () => {
