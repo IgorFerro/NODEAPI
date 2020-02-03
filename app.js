af1f449f86e3982ca3c3ef8363105447356f825e
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-02-03 07:20:04 -0300

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -16,31 +16,57 @@ app.post('/', (req,res)=>{
 })*/
 //Read the file
 const tours =JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+console.log(req.params);
 
 app.get('/api/v1/tours', (req,res) =>{
   res.status(200).json({
-      results: tours.length,
       status: 'sucess',
+      results: tours.length,
       data: {
           tours
       }
   })
 });
 
+//Search by id
+app.get('/api/v1/tours/:id', (req,res) =>{
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour =tours.find(el=> el.id === id);
+
+
+    //if(id > tours.length){
+        if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'sucess',
+        data: {
+            tour
+        }
+    });
+  });
+
+//Make a post
 app.post('/api/v1/tours', (req,res)=>{
     //console.log(req.body);
     const newId = tours[tours.length -1].id +1;
     const newTour = Object.assign({id:newId}, req.body);
 
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours.json`,JSON.stringify(tours), err =>{
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err =>{
     res.status(201),json({
         status: 'sucess',
         data:{
             tour: newTour
         }
     })
-  })
+  });
 })
 
 
