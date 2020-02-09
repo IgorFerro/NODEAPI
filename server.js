5e98cab2c48f20451d130f790d7b39bfd1d343f8
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-02-09 11:39:46 -0300

    Simple tour Model

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ mongoose.connect(DB, {
 })
 .then(() =>console.log('DB connection sucessfull'))
 
+const tourSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required:[true,'A tour must have a name'],
+    unique: true
+  },
+  rating:{
+    type:Number,
+    default: 4.5,
+  },
+  price:{
+    type:Number,
+    required:[true, 'A tour must have a price!']
+  }
+});
+
+const Tour = mongoose.model('Tour',tourSchema);
+
 
 console.log(app.get('env'));
 console.log(process.env);
