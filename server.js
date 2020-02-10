a3b6b420d8f952ff7dc7a9acb4b3bf9a6a50d828
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-02-10 12:39:03 -0300

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(DB, {
 })
 .then(() =>console.log('DB connection sucessfull'))
 
+//Create Schema
 const tourSchema = new mongoose.Schema({
   name:{
     type: String,
@@ -30,6 +31,20 @@ const tourSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model('Tour',tourSchema);
+
+//New instance of tour document
+const testTour = new Tour({
+  name: 'The Forest Hiker2',
+  rating: 4.7,
+  price: 497
+})
+
+//Save new item
+testTour.save().then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log('ERROR', err)
+})
 
 
 console.log(app.get('env'));
