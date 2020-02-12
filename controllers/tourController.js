9d1c49bcb5068c50e88e5b77a1f5d1eb10a129da
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-02-12 07:30:30 -0300

    Another Way of Creating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,5 @@
 const Tour =require('./../models/tourModel')
 
- //Middleware for check body post tour route
-exports.checkBody = (req,res,next)=>{
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        })
-    }
-    next();
-}
-
-
 exports.getAllTours =  (req,res) =>{
     console.log(req.requestTime)
     res.status(200).json({
@@ -36,8 +24,23 @@ exports.getAllTours =  (req,res) =>{
         }
     });
   };
+   //Create Tour
+  exports.createTour = async (req,res)=>{
+  try{
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+      status: 'success',
+      data:{
+          tour: newTour
+      }
+  });
+ }catch(err){
+     res.status(400).json({
+         status: 'fail',
+         message: 'Invalid data sent!'
 
-  exports.createTour = (req,res)=>{
+     })
+}
 };
 
 exports.updateTour = (req , res) => {
