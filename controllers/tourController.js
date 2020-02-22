da3e42539ae6a7382fd6150fbdb2bfc3c2340a02
ifparreira <ifparreira@pagseguro.com>
2020-02-22 06:57:08 -0300

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,17 @@ exports.getAllTours = async (req,res) =>{
         query = query.select('-__v');
     }
 
+    //4 PAGINATION
+     const page = req.query.page * 1 || 1
+     const limit = req.query.limit * 1 || 100
+     const skip = (page -1) * limit;
+
+     query = query.skip(skip).limit(limit);
+
+     if(req.query.page){
+         const numTours = await Tour.countDocuments();
+         if(skip > numTours) throw new Error('This page does not exits');
+     }
      /*const tours = await Tour.find()
     .where('duration')
     .equals('5')
