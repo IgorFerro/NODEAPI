25a1bc2788b6f7e5293a894b67e05dc31e8cab6f
ifparreira <ifparreira@pagseguro.com>
2020-02-22 17:23:05 -0300

    Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour =require('./../models/tourModel')
 
+exports.aliasTopTours = (req, res , next) =>{
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+
+};
+
 //Get All Tours
 exports.getAllTours = async (req,res) =>{
     try{
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 //Param middleware for get by id Tours
 //router.param('id',tourController.checkID);
 
+router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours);
+
+
 router
 .route('/')
 .get(tourController.getAllTours)
