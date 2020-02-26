058f5af70cee0e80ed16dacf53b922e05874b23d
ifparreira <ifparreira@pagseguro.com>
2020-02-26 06:48:39 -0300

    Mongoose Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify')
 
 //Create Schema
 const tourSchema = new mongoose.Schema({
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
       unique: true,
       trim: true
     },
+    slug: String,
     duration:{
      type: Number,
      required: [true,'A tour must have a duration']
@@ -60,7 +62,15 @@ createAt: {
 
   tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7;
+  });
+
+  //MONGOOSE DOCUMENT MIDDLEWARE: runs before .save() and .create()
+  tourSchema.pre('save',function(next){
+  this.slug=slugify(this.name,{lower: true})
+  next();
   })
+
+
 
   const Tour = mongoose.model('Tour',tourSchema);
 
