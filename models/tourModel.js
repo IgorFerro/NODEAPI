71a511df8a3ac270f7f046199511bc5cd4b0c742
ifparreira <ifparreira@pagseguro.com>
2020-02-26 07:14:52 -0300

    Mongoose Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,11 @@ createAt: {
   default: Date.now(),
   select: false
 },
- startDates:[Date]
+ startDates:[Date],
+ secretTour: {
+   type: Boolean,
+   default: false
+ }
 }, {
   toJSON: {virtuals:true},
   toObject: {virtuals:true}
@@ -68,6 +72,19 @@ createAt: {
   tourSchema.pre('save',function(next){
   this.slug=slugify(this.name,{lower: true})
   next();
+  });
+
+  //MONGOOSE QUERY MIDDLEWARE
+  tourSchema.pre(/^find/, function(next){
+    this.find({secretTour: {$ne: true}})
+    this.start =Date.now();
+    next();
+  });
+
+  tourSchema.post(/^find/, function(docs,next){
+    console.log(`Query took ${Date.now() - this.start} millesenconds `)
+    console.log(docs);
+    next();
   })
 
 
