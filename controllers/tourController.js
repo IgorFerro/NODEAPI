07ea5b5eea45d94579531ccbf093891e8140dd14
ifparreira <ifparreira@pagseguro.com>
2020-02-28 18:23:15 -0300

    Adding 404 Not Found Errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
-const Tour =require('./../models/tourModel')
-const APIFeatures = require('./../utils/apiFeatures')
-const catchAsync = require('./../utils/catchAsync')
+const Tour =require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res , next) =>{
     req.query.limit = '5';
@@ -9,10 +10,9 @@ exports.aliasTopTours = (req, res , next) =>{
     next();
 
 };
-
 //Get All Tours
 exports.getAllTours = catchAsync(async (req,res,next) =>{
-  
+
     //EXECUTE QUERY
     const features = new APIFeatures(Tour.find(),req.query)
     .filter()
@@ -33,7 +33,11 @@ exports.getAllTours = catchAsync(async (req,res,next) =>{
 
   //Get Tour
   exports.getTour = catchAsync(async (req,res,next) =>{
-       const tour = await Tour.findById(req.params.id);
+    const tour = await Tour.findById(req.params.id);
+      
+    if(!tour){
+      return next(new AppError('No Tour found with that ID', 404))
+    }
 
        res.status(200).json({
         status: 'sucess',
@@ -61,6 +65,10 @@ exports.updateTour = catchAsync(async (req , res, next) => {
        runValidators: true
    });
 
+   if(!tour){
+    return next(new AppError('No Tour found with that ID', 404))
+  }
+
    res.status(200).json({
        status: 'success',
        data:{
@@ -71,7 +79,12 @@ exports.updateTour = catchAsync(async (req , res, next) => {
 //Delete Tour
 
 exports.deleteTour= catchAsync(async (req , res, next) => {
-   await Tour.findByIdAndDelete(req.params.id)
+  const tour = await Tour.findByIdAndDelete(req.params.id)
+
+   if(!tour){
+    return next(new AppError('No Tour found with that ID', 404))
+  }
+
    res.status(204).json({
        status: 'success',
        data: null
