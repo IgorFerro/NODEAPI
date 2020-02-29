c1183298aa5b97481833ee80721f6174d3626d2d
ifparreira <ifparreira@pagseguro.com>
2020-02-29 05:34:35 -0300

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,11 @@ const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 }
+const handleDuplicateFieldsDB = err =>{
+  const value = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`
+  return new AppError(message, 400);
+}
 
 const sendErrorDev = (err,res) =>{
   res.status(err.statusCode).json({
@@ -43,11 +48,17 @@ module.exports = (err, req, res, next) => {
     err.status= err.status || 'error';
 
     if(process.env.NODE_ENV === 'development'){
-      sendErrorDev(err,res)
+      let error = {...err};
+      
+      if(error.name === 'CastError') error = handleCastErrorDB(error)
+      if(error.code ===11000) error = handleDuplicateFieldsDB(error)
+      sendErrorProd(error,res);
+      //sendErrorDev(err,res)
     } else if (process.env.NODE_ENV === 'production'){
       let error = {...err};
       
       if(error.name === 'CastError') error = handleCastErrorDB(error)
+      if(error.code ===11000) error = handleDuplicateFieldsDB(error)
       
       sendErrorProd(error,res);
     }
