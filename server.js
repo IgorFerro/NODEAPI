b8ebb921b65f4a1d1825bf50e21fbba8dcbf6204
ifparreira <ifparreira@pagseguro.com>
2020-02-29 06:10:08 -0300

    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ mongoose.connect(DB, {
 
 //START SERVER
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1)
+  })
 });
