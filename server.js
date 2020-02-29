c67123a2fefd0b992b4388a34f155a6a71da5697
ifparreira <ifparreira@pagseguro.com>
2020-02-29 06:22:40 -0300

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err =>{
+  console.log('UNCAUGHT REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+  });
+
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -21,9 +28,11 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1)
-  })
+  });
 });
+
+
