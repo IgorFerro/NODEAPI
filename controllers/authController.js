d99b21f7abaed1b2ddb16e57289972964bd3e442
IgorFerro <igor.ferro.parreira@gmail.com>
2020-03-04 04:50:36 -0300

    Password Reset Functionality: Reset Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,6 +93,25 @@ exports.restrictTo = (...roles) => {
             );
     };
     next();
-}
- }
+};
+ };
+exports.forgotPassword = catchAsync(async(req, res, next) => {
+    //1- Get user based on Posted email
+    const user = await User.findOne({email: req.body.email});
+    if(!user) {
+        return next(new AppError('There is no user with address.',404));
+        
+    }
+    //2- Genarate the random
+    const resetToken =user.createPasswordResetToken();
+    await user.save({validateBeforeSave: false});
+    //3- Send it to user's email
+
+});
+exports.resetPassword = (req, res,next) =>{};
+
+
+
+
+
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -39,7 +40,9 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangeAt: Date
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function(next) {
@@ -65,7 +68,18 @@ userSchema.methods.changePasswordAfter = function(JWTTimestamp){
         return JWTTimestamp < changedTimestamp;
     }
     return false;
-}
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+   const resetToken = crypto.randomBytes(32).toString('hex');  
+   
+   this.passwordResetToken =  crypto.createHash('sha256').update(resetToken).digest('hex');
+   console.log({resetToken}, this.passwordResetToken)
+
+   this.passwordResetExpires = Date.now() +10 *60 *1000;
+
+return resetToken;
+};
 
 //Create the model out of the schema
 const User = mongoose.model('User', userSchema);
