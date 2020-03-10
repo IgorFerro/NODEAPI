bff7cd3356f0a7b19910fb4e4c6afb5c0ea8a956
ifparreira <ifparreira@pagseguro.com>
2020-03-10 17:57:04 -0300

    Nested Routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController')
 const authController = require('./../controllers/authController')
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes')
 
 const router = express.Router();
 
 //Param middleware for get by id Tours
 //router.param('id',tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours);
 
@@ -24,13 +26,5 @@ router
 .patch(tourController.updateTour)
 .delete(authController.protect, authController.restrictTo('admin','lead-guide'),
 tourController.deleteTour);
-
-router
-.route('/:tourId/reviews')
-.post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-);
 
 module.exports = router;
