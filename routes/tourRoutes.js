3fc211206318a95669101672d1911a9518550dfe
ifparreira <ifparreira@pagseguro.com>
2020-03-10 17:42:38 -0300

    Implementing Simple Nested Routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController')
 const authController = require('./../controllers/authController')
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -23,5 +24,13 @@ router
 .patch(tourController.updateTour)
 .delete(authController.protect, authController.restrictTo('admin','lead-guide'),
 tourController.deleteTour);
+
+router
+.route('/:tourId/reviews')
+.post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const userController =require('../controllers/userController')
 const authController =require('../controllers/authController')
+
+
 const router = express.Router();
 
 
@@ -27,5 +29,7 @@ router
 .get(userController.getUser)
 .patch(userController.updateUser)
 .delete(userController.deleteUser);
+
+
 
 module.exports = router;
