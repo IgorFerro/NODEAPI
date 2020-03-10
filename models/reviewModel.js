5c7c939f3152c37ff1c412fc95fd54978918dc9f
ifparreira <ifparreira@pagseguro.com>
2020-03-10 17:20:18 -0300

    Virtual Populate: Tours and Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,10 +32,7 @@ const reviewSchema = new mongoose.Schema(
     });
 
     reviewSchema.pre(/^find/, function(next){
-        this.populate({
-            path: 'tour',
-            select: 'name'
-        }).populate({
+      this.populate({
             path: 'user',
             select: 'name photo'
         })
--- models/tourModel.js
+++ models/tourModel.js
@@ -119,6 +119,13 @@ createAt: {
     return this.duration / 7;
   });
 
+  // Virtual populate
+  tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id'
+  });
+
   //MONGOOSE DOCUMENT MIDDLEWARE: runs before .save() and .create()
   tourSchema.pre('save',function(next){
   this.slug=slugify(this.name,{lower: true})
