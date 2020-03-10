c390ebcaa2c086659cbdb391fb9c0fe4ea933948
ifparreira <ifparreira@pagseguro.com>
2020-03-10 08:18:10 -0300

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController')
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean');
 const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
 
 const app = express();
 
@@ -63,6 +65,7 @@ app.use((req, res, next)=>{
 //2- ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Handling Unhandled Routes
 app.all('*',(req, res, next)=>{
