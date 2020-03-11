01e3e44d3648fecac60dfaa1c0672235186f7fee
ifparreira <ifparreira@pagseguro.com>
2020-03-11 14:02:40 -0300

    Adding Missing Authentication and Authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,15 +11,18 @@ userController.getMe,
 userController.getUser)
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch('/updateMyPassword', authController.protect,authController.updatePassword);
+//Protect all routes after this middleware
+router.use(authController.protect);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
 
+router.patch('/updateMe',  userController.updateMe);
+router.delete('/deleteMe',  userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 //Routs
 router
