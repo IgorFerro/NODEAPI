2803acd504a0008e0ac460973cdda94cd6a688ab
ifparreira <ifparreira@pagseguro.com>
2020-03-11 07:12:09 -0300

    Some Changes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -58,7 +58,7 @@ module.exports = (err, req, res, next) => {
     //err.status= err.status || 'error';
 
     if(process.env.NODE_ENV === 'development'){
-      let error = {...err};
+      /*let error = {...err};
 
       if(error.name === 'CastError') error = handleCastErrorDB(error)
       if(error.code ===11000) error = handleDuplicateFieldsDB(error)
@@ -67,8 +67,8 @@ module.exports = (err, req, res, next) => {
       if (error.name === 'JsonWebTokenError') error = handleJWTError (error);
       if(error.name = 'TokenExpiredError') error = handleJWTExpiredError(error);
       
-      sendErrorDev(error,res)
-      //sendErrorDev(err,res)
+      sendErrorDev(error,res)*/
+      sendErrorDev(err,res)
     } else if (process.env.NODE_ENV === 'production'){
       let error = {...err};
       
