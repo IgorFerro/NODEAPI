1356b4b02edb1bd990d17a39b5a116eab91e7251
ifparreira <ifparreira@pagseguro.com>
2020-03-19 15:47:08 -0300

    Building the Tour Page - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,16 @@ const tours = await Tour.find();
     });
   });
 
-exports.getTour = (req, res) =>{
+exports.getTour = catchAsync(async(req, res) =>{
+   // 1- Get the data, for the requested tour (including reviews and guides)
+const tour = await (await Tour.findOne({slug: req.params.slug})).populated({
+  path: 'reviews',
+  fields: 'review rating user'
+});
+// 2- Build template
+// 3- Render template using data from 1-
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour'
   });
-};
+});
